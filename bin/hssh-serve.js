cc9131bba09b16491c2bd516dedd6467fffb2772
KeyangXiang <keyang.xiang@gmail.com>
2016-11-04 23:56:59 +0000

    add readme

--- bin/hssh-serve.js
+++ bin/hssh-serve.js
@@ -5,12 +5,9 @@ program.description("A simple http server enabled for hssh.");
 program.arguments("<port>");
 program.parse(process.argv);
 
-var port=parseInt(program.args[0]);
-if (!port){
-  program.help();
-  process.exit(1);
-}
+var port=parseInt(program.args[0]) || process.env.PORT || 8010;
 var server=require("http").createServer();
 require("../")(server);
+console.log("hssh-serve is only used for testing hssh. For production usage for http servers, please see https://github.com/Keyang/hssh");
 console.log("Server listen on: ",port);
 server.listen(port);
